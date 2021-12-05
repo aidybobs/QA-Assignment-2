703dc391e95a556852b05d721982770f052f3eb2
aidybobs <aidanstark@hotmail.com>
2021-12-05 20:34:18 +0000

    changed tests.sh

--- tests.sh
+++ tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt install python3 python3-pip python3-venv -python3
+sudo apt update
+sudo apt install python3 python3-pip python3-venv -python3
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r testrequirements.txt
