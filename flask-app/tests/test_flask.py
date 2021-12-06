edca726d60a3a0f53033736311a14cc38ca4a084
aidybobs <aidanstark@hotmail.com>
2021-12-06 01:06:39 +0000

    fixing Jenkinsfile tests

--- flask-app/tests/test_flask.py
+++ flask-app/tests/test_flask.py
@@ -1,4 +1,5 @@
-from application import app
+from application import app, db
+from application.models import Character
 from flask import url_for
 from flask_testing import TestCase
 import requests_mock
@@ -8,17 +9,28 @@ import requests
 class TestBase(TestCase):
     def create_app(self):
         app.config.update(
-            SQLALCHEMY_DATAVBASE_URI='sqlite:///test.db',
+            SQLALCHEMY_DATABASE_URI='sqlite:///test.db',
             DEBUG=True,
         )
         return app
+
+    def setUp(self):
+        db.create_all()
+        sampleemp = Character(name='James', race='Human', archetype='Warrior')
+        db.session.add(sampleemp)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 
 class TestResponse(TestBase):
     def test_home(self):
         response = self.client.get(url_for('generate'))
         self.assertEquals(response.status_code, 200)
-        self.assertIn(b'Skyrim', response.data)
+        self.assertIn(b'James', response.data)
+        self.assertIn(b'Human', response.data)
 
     def test_new(self):
         with requests_mock.Mocker() as m:
