400ed2d92c57a2ce3dd22451ce5ce9386c47844d
aidybobs <aidanstark@hotmail.com>
2021-12-06 12:41:33 +0000

    fixing tests.sh

--- tests.sh
+++ tests.sh
@@ -3,8 +3,8 @@ declare -a directories=("archetype" "race" "character" "flask-app")
 for dir in "${directories[@]}"
 do
   cd ${dir}
-  sudo apt-get update
-  sudo apt-get install python3 python3-pip python3-venv
+  sudo apt-get update -y
+  sudo apt-get install python3 python3-pip python3-venv -y
   python3 -m venv venv
   SOURCE venv/bin/activate
   pip3 install -r testrequirements.txt
