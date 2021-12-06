d9db22c0bf741cc3902d0141147dcd7bac880fc0
aidybobs <aidanstark@hotmail.com>
2021-12-06 00:22:21 +0000

    fixing Jenkinsfile tests

--- tests.sh
+++ tests.sh
@@ -3,14 +3,12 @@ declare -a directories=("archetype" "race" "character" "flask-app")
 for dir in "${directories[@]}"
 do
   cd ${dir}
-  cd tests
   sudo apt-get update
   sudo apt-get install python3 python3-pip python3-venv
   python3 -m venv venv
   . venv/bin/activate
   pip3 install -r testrequirements.txt
-  python3 -m pytest --cov=application --cov-report=term
+  python3 -m pytest tests/tests.py --cov=application --cov-report=term
   deactivate
-  cd ..
   cd ..
 done
