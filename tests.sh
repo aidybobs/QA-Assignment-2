d25c30eceaf4f3e09418bb311ce1db86fefc752d
aidybobs <aidanstark@hotmail.com>
2021-12-06 00:07:50 +0000

    fixing Jenkinsfile tests

--- tests.sh
+++ tests.sh
@@ -7,7 +7,7 @@ do
   sudo apt-get install python3 python3-pip python3-venv
   python3 -m venv venv
   . venv/bin/activate
-  pip3 install -r test_requirements.txt
+  pip3 install -r testrequirements.txt
   python3 -m pytest --cov=application --cov-report=application
   deactivate
   cd ..
